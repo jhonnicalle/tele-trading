7de6a615ce2824724633951e13b1b730aff44f01
Jhonni Calle <jhonnicalle@gmail.com>
2020-10-21 15:11:28 -0400

    Updates

---
+++ precache-manifest.5320661b0236ceaada7ddd5bf111bd45.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74e241ca9fead7bc20570ef6636e378",
+    "url": "/tele-trading/index.html"
+  },
+  {
+    "revision": "edc26f3357bc4adc474d",
+    "url": "/tele-trading/static/css/main.4b3daf16.chunk.css"
+  },
+  {
+    "revision": "81a85e087632ee99cd8d",
+    "url": "/tele-trading/static/js/2.5cd05173.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/tele-trading/static/js/2.5cd05173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc26f3357bc4adc474d",
+    "url": "/tele-trading/static/js/main.cf43511b.chunk.js"
+  },
+  {
+    "revision": "d49b4fea78871fbe8be8",
+    "url": "/tele-trading/static/js/runtime-main.b1381e78.js"
+  },
+  {
+    "revision": "55195ee2581f942921ecbee7d2b60301",
+    "url": "/tele-trading/static/media/CONTACT US BACKGROUND.55195ee2.jpg"
+  },
+  {
+    "revision": "b12b3430370729808f4513a20d512981",
+    "url": "/tele-trading/static/media/HEADER BACKGROUND.b12b3430.jpg"
+  }
+]);
