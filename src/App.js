a438997291e2ad17382e83e6112e12873bbe0f26
Jhonni Calle <jhonni.calle@xyclon.com.ar>
2020-12-06 15:43:16 -0400

    change in <App />

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import React from 'react';
 import { THEME } from './AppStyles';
 // import Navbar from './components/Navbar/Navbar';
 import Navbar2 from './components/Navbar/Navbar2';
-// import ContactUs from './components/ContactUs/ContactUs';
-// import Guarantee from './components/Guarantee/Guarantee';
-// import Header from './components/Header/Header';
-// import Map from './components/Map/Map';
-// import Services from './components/Services/Services';
-// import Values from './components/Values/Values';
 
 const App = () => {
   return (
